55e67a5edadb81046db4c0d19a3126e1f5978ecc
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-02-17 14:11:35 +0200

    button: focus mode active/inactive switch-design

--- src/apps/Popup.vue
+++ src/apps/Popup.vue
@@ -25,13 +25,69 @@ export default {
 </script>
 
 <template>
-  <v-layout>
-    <v-btn v-if="appDataStore.getAppData.focusModeActive" @click="appDataStore.updateFocusModeActive(false)">Off</v-btn>
-    <v-btn v-if="!appDataStore.getAppData.focusModeActive" @click="appDataStore.updateFocusModeActive(true)">On</v-btn>
-    <v-btn @click="options">Settings</v-btn>
-  </v-layout>
+  <v-row>
+    <v-col>
+      <h1>Focus Mode</h1>
+    </v-col>
+  </v-row>
+  <v-row class="on-off-button-group">
+    <v-col cols="6">
+      <v-btn
+        color="danger"
+        class="button-off"
+        :class="{'button-off-outlined': appDataStore.getAppData.focusModeActive}"
+        :variant="appDataStore.getAppData.focusModeActive ? 'outlined' : 'flat'"
+        @click="appDataStore.updateFocusModeActive(false)">
+        Off
+      </v-btn>
+    </v-col>
+    <v-col cols="6">
+      <v-btn
+        color="success"
+        class="button-on"
+        :class="{'button-on-outlined': !appDataStore.getAppData.focusModeActive}"
+        :variant="!appDataStore.getAppData.focusModeActive ? 'outlined' : 'flat'"
+        @click="appDataStore.updateFocusModeActive(true)">
+        On
+      </v-btn>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col>
+      <v-btn @click="options">Settings</v-btn>
+    </v-col>
+  </v-row>
 </template>
 
 <style scoped lang="scss">
+.button-off {
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+  width: 100%;
+}
 
+.button-on {
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  width: 100%;
+}
+
+.button-on-outlined {
+  border-left: 0;
+}
+
+.button-off-outlined {
+  border-right: 0;
+}
+
+.on-off-button-group {
+  display: flex;
+  flex-direction: row;
+  //justify-content: center;
+  //align-items: center;
+  margin: 0 1rem;
+  > * {
+    padding: 1rem 0;
+  }
+}
 </style>
