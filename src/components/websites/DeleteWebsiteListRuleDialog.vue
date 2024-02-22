0f3bd5b2d214e3c87d4b06692be0cbb6c874aee9
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-02-22 22:59:25 +0200

    refactoring composition api, delete list of rules dialog

--- src/components/websites/DeleteWebsiteListRuleDialog.vue
+++ src/components/websites/DeleteWebsiteListRuleDialog.vue
@@ -1,54 +1,45 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { computed, ref, watch } from 'vue';
 import { IWebsiteRuleList } from '@/interfaces';
 
-export default defineComponent({
-  name: 'DeleteWebsiteListRuleDialog',
-  props: {
-    pDialog: {
-      type: Boolean,
-      required: true
-    },
-    pItem: {
-      type: Object as () => IWebsiteRuleList,
-      required: true
-    },
-    pCloseDialog: {
-      type: Function,
-      required: true
-    },
-    pDeleteItemConfirm: {
-      type: Function,
-      required: true
-    },
-    pIsEmpty: {
-      type: Boolean,
-      required: true
-    }
+const props = defineProps({
+  pDialog: {
+    type: Boolean,
+    required: true
   },
-  data: () => {
-    return {
-      item: {} as IWebsiteRuleList,
-      isEmpty: false,
-      dialog: false,
-      confirmDeleteListItems: false
-    };
+  pItem: {
+    type: Object as () => IWebsiteRuleList,
+    required: true
   },
-  watch: {
-    pDialog(val: boolean) {
-      this.dialog = val;
-      if (val) {
-        this.isEmpty = this.pIsEmpty;
-        this.item = this.pItem;
-      }
-    }
+  pCloseDialog: {
+    type: Function,
+    required: true
   },
-  computed: {
-    eligibleForDeletion(): boolean {
-      return !this.isEmpty ? this.confirmDeleteListItems : true;
-    }
+  pDeleteItemConfirm: {
+    type: Function,
+    required: true
+  },
+  pIsEmpty: {
+    type: Boolean,
+    required: true
   }
 });
+
+let item = ref({} as IWebsiteRuleList);
+let dialog = ref(false);
+let confirmDeleteListItems = ref(false);
+let isEmpty = ref(false);
+
+const eligibleForDeletion = computed(() => !isEmpty.value ? confirmDeleteListItems.value : true);
+
+watch(() => props.pDialog, (value) => {
+  dialog.value = value;
+  if (value) {
+    item.value = props.pItem;
+    isEmpty.value = props.pIsEmpty;
+  }
+});
+
 </script>
 
 <template>
