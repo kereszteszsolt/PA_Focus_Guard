8548a9e0b5584e7cef4f66fda81aeb8b56fe1cd4
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-02-29 15:25:22 +0200

    initialize other views

--- src/router/optionsRouter.ts
+++ src/router/optionsRouter.ts
@@ -1,45 +1,78 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import { Websites, Test } from '@/views/options';
-import FocusMessage from '@/views/options/FocusMessage.vue';
-import Language from '@/views/options/Language.vue';
+import {
+  About,
+  Acknowledgments,
+  FocusMessage,
+  Gtc,
+  HowToUse,
+  Language,
+  NotFound,
+  Settings,
+  Test,
+  Websites,
+} from '@/views/options';
 
-export default createRouter(
+export default createRouter({
+  history: createWebHashHistory(),
+  routes: [
     {
-      history: createWebHashHistory(),
-      routes: [
-        {
-          path: '/websites',
-          name: 'WebsitesDefaultList',
-          component: Websites
-        },
-        {
-          path: '/websites/:id',
-          name: 'WebsitesByListId',
-          component: Websites
-        },
-        {
-          path: '/languages',
-          name: 'Languages',
-          component: Language
-        },
-        {
-          path: '/languages/:id',
-          name: 'LanguagesByLanguageId',
-          component: Language
-        },
-        {
-          path: '/focus-message',
-          name: 'FocusMessage',
-          component: FocusMessage
-        },
-        {
-          path: '/test',
-          name: 'Test',
-          component: Test
-        },
-        {
-          path: '/',
-          redirect: '/websites'
-        }]
-    }
-  );
+      path: '/about',
+      name: 'About',
+      component: About,
+    },
+    {
+      path: '/acknowledgments',
+      name: 'Acknowledgments',
+      component: Acknowledgments,
+    },
+    {
+      path: '/focus-message',
+      name: 'FocusMessage',
+      component: FocusMessage,
+    },
+    {
+      path: '/gtc',
+      name: 'Gtc',
+      component: Gtc,
+    },
+    {
+      path: '/how-to-use',
+      name: 'HowToUse',
+      component: HowToUse,
+    },
+    {
+      path: '/languages',
+      name: 'Languages',
+      component: Language,
+    },
+    {
+      path: '/settings',
+      name: 'Settings',
+      component: Settings,
+    },
+    {
+      path: '/websites',
+      name: 'WebsitesDefaultList',
+      component: Websites,
+    },
+    {
+      path: '/websites/:id',
+      name: 'WebsitesByListId',
+      component: Websites,
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test,
+    },
+    {
+      path: '/',
+      redirect: '/websites',
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound,
+    },
+  ],
+});
--- src/views/options/index.ts
+++ src/views/options/index.ts
@@ -1,4 +1,12 @@
-import Websites from './Websites.vue';
+import About from './About.vue';
+import Acknowledgments from './Acknowledgments.vue';
+import FocusMessage from './FocusMessage.vue';
+import Gtc from './Gtc.vue';
+import HowToUse from './HowToUse.vue';
+import Language from './Language.vue';
+import NotFound from './NotFound.vue';
+import Settings from './Settings.vue';
 import Test from './Test.vue';
+import Websites from './Websites.vue';
 
-export { Websites, Test };
+export { About, Acknowledgments, FocusMessage, Gtc, HowToUse, Language, NotFound, Settings, Test, Websites };
