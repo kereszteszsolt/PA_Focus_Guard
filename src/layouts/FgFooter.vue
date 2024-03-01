cd516922636c301b97a984d556219b24e11432b2
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-03-01 07:20:03 +0200

    footer pages setup 3

--- src/layouts/FgFooter.vue
+++ src/layouts/FgFooter.vue
@@ -34,12 +34,12 @@
         <v-icon>mdi-tree</v-icon>
       </v-btn>
     </div>
-    <div class="d-flex flex-row justify-space-around text-none text-center">
-      <rounter-link to="/about" class="flex-1-0"><span>About</span></rounter-link>
-      <rounter-link to="/Gtc" class="flex-1-0"><span>Gtc</span></rounter-link>
-      <rounter-link to="/donation" class="flex-1-0"><span>Donation</span></rounter-link>
-      <rounter-link to="/acknowledgments" class="flex-1-0"><span>Acknowledgments</span></rounter-link>
-      <rounter-link to="/how-to-use" class="flex-1-0"><span>How to use</span></rounter-link>
+    <div class="d-flex flex-row justify-space-around">
+          <router-link to="/about" class="text-none text-center flex-1-0">About</router-link>
+          <router-link to="/gtc" class="text-none text-center flex-1-0">Gtc</router-link>
+          <router-link to="/donation" class="text-none text-center flex-1-0">Donation</router-link>
+          <router-link to="/acknowledgment" class="text-none text-center flex-1-0">Acknowledgment</router-link>
+          <router-link to="/how-to-use" class="text-none text-center flex-1-0">How to use</router-link>
     </div>
     <div class="d-flex flex-row justify-space-around text-center">
       <p>Focus Guard © 2024 - Keresztes Zsolt - Version: 2.0.0 - Free Software.</p>
