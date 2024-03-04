763e7cfdc4f7c57c7e6a3d51acac01cd2d9af361
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-03-04 13:47:27 +0200

    refactoring

--- src/background.ts
+++ src/background.ts
@@ -64,46 +64,19 @@ chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
   console.log('Tab updated');
   await redirectOrAllow(details.tabId, details.url);
 });
-chrome.webNavigation.onCreatedNavigationTarget.addListener(async (details) => {
-  console.log('onCreatedNavigationTarget');
-});
-chrome.webNavigation.onCommitted.addListener(async (details) => {
-  console.log('onCommitted');
-});
-chrome.webNavigation.onDOMContentLoaded.addListener(async (details) => {
-  console.log('onDOMContentLoaded');
-});
+
 chrome.webNavigation.onHistoryStateUpdated.addListener(async (details) => {
   console.log('onHistoryStateUpdated');
   await redirectOrAllow(details.tabId, details.url);
 });
-chrome.webNavigation.onReferenceFragmentUpdated.addListener(async (details) => {
-  console.log('onReferenceFragmentUpdated');
-});
-chrome.webNavigation.onTabReplaced.addListener(async (details) => {
-  console.log('onTabReplaced');
-});
-chrome.webNavigation.onCompleted.addListener(async (details) => {
-  console.log('onCompleted');
-});
-chrome.webNavigation.onErrorOccurred.addListener(async (details) => {
-  console.log('onErrorOccurred');
-});
 
 const redirectOrAllow = async (tabId: number, url: string): Promise<void> => {
   if (url) {
-
+  console.log('url', url);
     let active = calculateActiveWebsiteRules();
     let relevant = active.filter((wr) => url && url.includes(wr.url));
-    let alreadyInAndTime = taskQueue.some((tq) => tq.tabId === tabId && tq.url === url && (Date.now() - tq.tabUpdatedTime) < 1500);
 
-    let focusPage = fgAppData.focusMode && url.includes('chrome-extension://') && url.includes('options.html#/focus-message');
-    if (focusPage) {
-      taskQueue = taskQueue.filter((tq) => tq.tabId !== tabId);
-    }
-
-    console.log('alreadyInAndTime', alreadyInAndTime);
-    if (relevant.length > 0 && !alreadyInAndTime && !focusPage) {
+    if (relevant.length > 0) {
       console.log('Relevant tab');
 
       taskQueue.push({
@@ -113,17 +86,11 @@ const redirectOrAllow = async (tabId: number, url: string): Promise<void> => {
         tabUpdatedTime: Date.now()
       });
 
-      console.log('chrome.tabs.onUpdated.addListener taskQueue:');
-      for (let task of taskQueue) {
-        console.log(task);
-      }
-
       await saveDistractionAttempt(relevant, fgAppData);
       await chrome.tabs.update(tabId, { url: '/options.html#/focus-message' });
     }
   }
 }
-
 
 chrome.runtime.onStartup.addListener(async () => {
   console.log('Runtime started');
