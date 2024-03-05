39641ea49a0f954962dc435109ebac6715d064b5
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-03-05 14:50:57 +0200

    fix messages / runtime last-error

--- src/utils/runtime-messages.ts
+++ src/utils/runtime-messages.ts
@@ -19,7 +19,11 @@ export const createBatchMessageListenerM2O = (message: string[], action: Functio
 }
 
 export const sendMessage = (message: string, callback?: Function) => {
-  chrome.runtime.sendMessage({ message: message }, function(response) {
-    if (callback) callback(response);
-  });
+    chrome.runtime.sendMessage({ message: message }, function(response) {
+      if (chrome.runtime.lastError) {
+        // !avoid error when options page is not open!
+        // console.log('Unchecked runtime.lastError: Could not establish connection. Receiving end does not exist.')
+      }
+      if (callback) callback(response);
+    });
 };
