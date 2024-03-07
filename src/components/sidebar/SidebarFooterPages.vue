c73847028222a78dcfd0d68ff31ac12abcdb3fef
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-03-07 08:03:17 +0200

    sidebar footer items menu part 2

--- src/components/sidebar/SidebarFooterPages.vue
+++ src/components/sidebar/SidebarFooterPages.vue
@@ -1,37 +1,22 @@
 <script setup lang="ts">
-
+const footerItems = [
+  { title: 'About', componentName: 'About', key: 'about' },
+  { title: 'Gtc', componentName: 'Gtc', key: 'gtc' },
+  // { title: 'Privacy', componentName: 'Privacy', key: 'privacy' },
+  // { title: 'Contact', componentName: 'Contact', key: 'contact' },
+  { title: 'Acknowledgments', componentName: 'Acknowledgments', key: 'acknowledgments' },
+  { title: 'HowToUse', componentName: 'HowToUse', key: 'how-to-use' },
+  { title: 'Donations', componentName: 'Donations', key: 'donations' }
+];
 </script>
 
 <template>
   <v-sheet class="border-radius-8 flex-1-0" color="background">
     <v-list class="bg-background border-radius-8">
-      <router-link :key="'about'" :to="{name: 'About'}" class="router-link text">
-        <v-list-item color="primary" :value="'about'">
-          <v-list-item-title>About</v-list-item-title>
-        </v-list-item>
-      </router-link>
-
-      <router-link :key="'gtc'" :to="{name: 'Gtc'}" class="router-link text">
-        <v-list-item color="primary" :value="'gtc'">
-          <v-list-item-title>GTC</v-list-item-title>
-        </v-list-item>
-      </router-link>
-
-      <router-link :key="'acknowledgements'" :to="{name: 'Acknowledgements'}" class="router-link text">
-        <v-list-item color="primary" :value="'acknowledgements'">
-          <v-list-item-title>Acknowledgements</v-list-item-title>
-        </v-list-item>
-      </router-link>
-
-      <router-link :key="'how-to-use'" :to="{name: 'HowToUse'}" class="router-link text">
-        <v-list-item color="primary" :value="'how-to-use'">
-          <v-list-item-title>How to use</v-list-item-title>
-        </v-list-item>
-      </router-link>
-
-      <router-link :key="'donations'" :to="{name: 'Donations'}" class="router-link text">
-        <v-list-item color="primary" :value="'donations'">
-          <v-list-item-title>Donations</v-list-item-title>
+      <router-link
+        v-for="item in footerItems" :key="item.key" :to="{name: item.componentName}" class="router-link text">
+        <v-list-item color="primary" :value="item.key">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item>
       </router-link>
     </v-list>
