a207a6f28afda6733d7f1edab55021d12d1d8a05
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-03-07 07:42:02 +0200

    sidebar footer items menu

--- src/components/sidebar/SidebarFooterPages.vue
+++ src/components/sidebar/SidebarFooterPages.vue
@@ -5,16 +5,42 @@
 <template>
   <v-sheet class="border-radius-8 flex-1-0" color="background">
     <v-list class="bg-background border-radius-8">
-      <v-list-item>
-        <template v-slot:prepend>
-          <v-btn density="compact" icon="mdi-plus" color="accent" variant="elevated" elevation="4"></v-btn>
-        </template>
-      </v-list-item>
-      <v-divider></v-divider>
+      <router-link :key="'about'" :to="{name: 'About'}" class="router-link text">
+        <v-list-item color="primary" :value="'about'">
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item>
+      </router-link>
+
+      <router-link :key="'gtc'" :to="{name: 'Gtc'}" class="router-link text">
+        <v-list-item color="primary" :value="'gtc'">
+          <v-list-item-title>GTC</v-list-item-title>
+        </v-list-item>
+      </router-link>
+
+      <router-link :key="'acknowledgements'" :to="{name: 'Acknowledgements'}" class="router-link text">
+        <v-list-item color="primary" :value="'acknowledgements'">
+          <v-list-item-title>Acknowledgements</v-list-item-title>
+        </v-list-item>
+      </router-link>
+
+      <router-link :key="'how-to-use'" :to="{name: 'HowToUse'}" class="router-link text">
+        <v-list-item color="primary" :value="'how-to-use'">
+          <v-list-item-title>How to use</v-list-item-title>
+        </v-list-item>
+      </router-link>
+
+      <router-link :key="'donations'" :to="{name: 'Donations'}" class="router-link text">
+        <v-list-item color="primary" :value="'donations'">
+          <v-list-item-title>Donations</v-list-item-title>
+        </v-list-item>
+      </router-link>
     </v-list>
   </v-sheet>
 </template>
 
 <style scoped lang="scss">
-
+.router-link {
+  text-decoration: none;
+  color: inherit;
+}
 </style>
