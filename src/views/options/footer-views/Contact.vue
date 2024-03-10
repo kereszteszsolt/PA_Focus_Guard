77040cf07a56b5f73badda56e04787783251c416
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-03-10 17:42:24 +0200

    contact...

--- src/views/options/footer-views/Contact.vue
+++ src/views/options/footer-views/Contact.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { FooterViewWrapper } from '@/views/options';
+import { socialMediaLinks } from '@/links';
 </script>
 
 <template>
   <footer-view-wrapper title="Contact">
-    <h1>Contact</h1>
+    <v-btn v-for="link in socialMediaLinks" variant="text" class="flex-1-0"
+           color="info" density="compact">
+      <v-icon>{{ link.mdiIcon }}</v-icon>
+    </v-btn>
   </footer-view-wrapper>
 </template>
 
