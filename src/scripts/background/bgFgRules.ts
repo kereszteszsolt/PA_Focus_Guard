894e1cd6fc330a0b6a268927760d34c16bf449ad
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-03-15 07:20:41 +0200

    prevent extension blocking itself

--- src/scripts/background/bgFgRules.ts
+++ src/scripts/background/bgFgRules.ts
@@ -80,8 +80,10 @@ export const applyRulesOnOpenTabs = async (fgAppData: IAppData, fgWebsiteRules: 
   chrome.tabs.query({}, (tabs) => {
     tabs.forEach((tab: chrome.tabs.Tab) => {
       const itemsToBlock = fgWebsiteRules.filter((wr) => !wr.temporarilyInactive && (wr.permanentlyActive || fgAppData.focusMode));
+      const extensionId = chrome.runtime.id
+      const extensionOptionsPath = `chrome-extension://${extensionId}/options.html#/`
       itemsToBlock.forEach((item) => {
-        if (tab.url && tab.url.includes(item.urlFilter) && tab.id) {
+        if (tab.url && tab.url.includes(item.urlFilter) && tab.id && !tab.url.includes(extensionOptionsPath)) {
           chrome.tabs.update(tab.id, { url: '/options.html#/focus-message' });
         }
       });
