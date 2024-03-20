6f9fc360a61ee8e792bbaa022f95636cab1382fb
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-03-20 14:06:27 +0200

    acknowledgements

--- src/views/options/footer-views/Acknowledgments.vue
+++ src/views/options/footer-views/Acknowledgments.vue
@@ -13,21 +13,21 @@ const tr = (key: string) => computed(() => i18n.getRestrictedTranslation(key)).v
 </script>
 
 <template>
-  <footer-view-wrapper :title="tr(r_msg.ACKNOWLEDGMENTS)">
+  <footer-view-wrapper :title="tr(r_msg.ACKNOWLEDGMENTS)" class="footer-view">
     <v-list lines="two" class="bg-background">
-      <v-list-item v-for="acknowledgement in acknowledgementArray" :key="acknowledgement.id" >
+      <v-list-item v-for="acknowledgement in acknowledgementArray" :key="acknowledgement.id" class="list-item">
         <v-list-item-content>
-          <v-list-item-title class="font-weight-bold fgc-primary mb-2">{{acknowledgement.title}}</v-list-item-title>
-          <p>{{acknowledgement.text}}</p>
-          <div v-if="acknowledgement.images">
+          <v-list-item-title class="font-weight-bold fgc-primary mb-2 title">{{acknowledgement.title}}</v-list-item-title>
+          <p class="text">{{acknowledgement.text}}</p>
+          <div v-if="acknowledgement.images" class="image-container">
             <v-list-item-subtitle>{{acknowledgement.textAllImg}}</v-list-item-subtitle>
-            <div v-for="image in acknowledgement.images" :key="image.src">
+            <div v-for="image in acknowledgement.images" :key="image.src" class="image-item">
               <p>{{image?.description}}</p>
               <v-img :src="image.src" :alt="image.alt" width="300"></v-img>
             </div>
           </div>
-          <div v-if="acknowledgement.links">
-            <v-list-item-subtitle v-for="link in acknowledgement.links" :key="link.url">
+          <div v-if="acknowledgement.links" class="link-container">
+            <v-list-item-subtitle v-for="link in acknowledgement.links" :key="link.url" class="link-item">
               <p class="fgc-secondary fg-font-s-16">{{link.text}}</p>
               <a :href="link.url" target="_blank" class="fgc-info">{{link.url}}</a>
             </v-list-item-subtitle>
@@ -39,5 +39,38 @@ const tr = (key: string) => computed(() => i18n.getRestrictedTranslation(key)).v
 </template>
 
 <style scoped lang="scss">
+.footer-view {
+  padding: 20px;
+  background-color: #f5f5f5;
+}
 
+.list-item {
+  margin-bottom: 20px;
+  border-bottom: 1px solid #ddd;
+  padding-bottom: 20px;
+}
+
+.title {
+  font-size: 1.5em;
+}
+
+.text {
+  margin-bottom: 10px;
+}
+
+.image-container {
+  margin-top: 10px;
+}
+
+.image-item {
+  margin-bottom: 10px;
+}
+
+.link-container {
+  margin-top: 10px;
+}
+
+.link-item {
+  margin-bottom: 10px;
+}
 </style>
