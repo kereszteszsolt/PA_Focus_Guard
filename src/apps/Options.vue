9cdbab1d9aabfd3a4a0dd26b1c7acf7c5840e8b1
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2024-03-25 19:35:20 +0200

    refactoring / fix screen 03

--- src/apps/Options.vue
+++ src/apps/Options.vue
@@ -101,10 +101,10 @@ utils.runtimeMessages.createMessageListener('appDataUpdated', () => {
   //  max-width: 720px;
   //}
 
-  @media (min-width: 992px) {
-    max-width: 960px;
-  }
-
+  //@media (min-width: 992px) {
+  //  max-width: 960px;
+  //}
+  //
   @media (min-width: 1200px) {
     max-width: 1140px;
   }
